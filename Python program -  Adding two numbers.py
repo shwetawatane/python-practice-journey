300fddcec29cdf6779c37f50f9874d26ad301abb
shwetawatane <163028552+shwetawatane@users.noreply.github.com>
2026-05-26 23:57:25 +0530

    Add files via upload

---
+++ Python program -  Adding two numbers.py
@@ -0,0 +1,20 @@
+#adding two numbers
+
+#solution 01 (with pre-defined variable)
+
+num1 = 10
+num2 = 30
+sum = num1 + num2
+print("the sum of the two number is ", sum)
+
+
+#solution 02 (with user-input)
+
+num1 = float(input("Enter the first number: "))
+num2 =float(input("Enter the second number: "))
+
+sum = num1 + num2
+
+print("the sum of the two number is ", sum)
+
+
